05fb29cf7d5d2a8f8eb31af15189e421c7db2f58
DGunnery05 <dg508@kent.ac.uk>
2024-10-27 21:34:52 +0000

    One

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,8 @@
-### Part Four -- your code goes here. 
+import random
+numbers = [random.randint(1, 100) for _ in range(10)]
+print("Original list:", numbers)
+for number in numbers[:]:
+    if number % 2 == 0:
+        while number in numbers:
+            numbers.remove(number)
+print("List with only odd numbers:", numbers)
